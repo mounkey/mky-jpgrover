8e01d18e3a5ff7888c83c1042914e7ab4406446d
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <jpgrover@mounkey.cl>
2022-06-14 14:56:38 -0400

    Arreglo menu

--- src/componets/NavBar.js
+++ src/componets/NavBar.js
@@ -3,18 +3,18 @@ import 'materialize-css/dist/css/materialize.css';
 import logo from '../logo.svg';
 import M from 'materialize-css';
 
-export default class componentName extends Component {
+export default class Navbar extends Component {
   componentDidMount(){
     const elemts = document.querySelector('.sidenav');
     M.Sidenav.init(elemts, {});
   }
-  render() {
+  render() {  
     return (
-      <div> 
+      <>
         <nav>
           <div className='nav-wrapper'>
             <a href="#!" className='brand-logo'><img src={logo} className="App-logo2" alt="logo" /></a>
-            <a href="#!" data-target="mobile-demo" className='sidenav-trigger'><i class="material-icons">menu</i></a>
+            <a href="#!" data-target="mobile-demo" className='sidenav-trigger'><i className='material-icons'>menu</i></a>
             <ul className='right hide-on-med-and-down'>
               <li><a href="#!">Index</a></li>
               <li><a href="#!">Como comprar</a></li>     
@@ -24,13 +24,13 @@ export default class componentName extends Component {
           </div>
         </nav>
 
-        <ul className='sidenav' id="mobile-demo">
-          <li><a href="sass.html">Sass</a></li>
-          <li><a href="badges.html">Components</a></li>
-          <li><a href="collapsible.html">Javascript</a></li>
-          <li><a href="mobile.html">Mobile</a></li>
+        <ul className='sidenav red ' id="mobile-demo">
+          <li><a href="#!">Index</a></li>
+          <li><a href="#!">Como comprar</a></li>     
+          <li><a href= "#!">Licencias</a></li>
+          <li><a href= "#!">Juegos</a></li>
         </ul>
-      </div>
+      </>
     );
   }
 }
