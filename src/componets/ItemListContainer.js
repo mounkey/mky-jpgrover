4b43e9b33060ca210261c1543495e8505ffe1f0d
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <jpgrover@mounkey.cl>
2022-06-19 21:55:50 -0400

    fix state and function map

--- src/componets/ItemListContainer.js
+++ src/componets/ItemListContainer.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import '../App.css';
 import 'materialize-css/dist/css/materialize.css';  
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
@@ -6,15 +6,16 @@ import {faGlobe} from '@fortawesome/free-solid-svg-icons';
 
 
 const ItemListContainer =() =>  {
-  const masVendido= ['Windows 11', 'Windows 10', 'Office 365', 'Office 2022'];
+  const [masVendido]= useState(['Windows 11', 'Windows 10', 'Office 365', 'Office 2022']); 
+  console.log(masVendido);
   return(
     <div className="listado">
       <ul className="list">
         {
-        masVendido.map((items) => (
-          <li><a href="#!" key ={items.id} > <FontAwesomeIcon icon={faGlobe}/> {items}</a></li>
-        ))
-      }
+          masVendido.map((items) => (
+            <li key ={items.id} ><a href="#!" > <FontAwesomeIcon icon={faGlobe}/> {items}</a></li>
+          ))
+        }
       </ul>
     </div>
     )}
