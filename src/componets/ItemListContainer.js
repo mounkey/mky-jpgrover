78ccd8cc4f84c962c355971d2d808d78b861715f
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <jpgrover@mounkey.cl>
2022-06-23 17:01:59 -0400

    fix list and count

--- src/componets/ItemListContainer.js
+++ src/componets/ItemListContainer.js
@@ -6,13 +6,13 @@ import {faGlobe} from '@fortawesome/free-solid-svg-icons';
 
 
 const ItemListContainer =() =>  {
-  const [masVendido]= useState([{id: 1, prod: 'Windows 11'}, {id: 2, prod: 'Windows 10'}, {id:3, prod: 'Microsoft 365'}, {id: 4, prod: 'Office 2022'}])
+  const [masVendido,setmasVendido] = useState([{id: 1, prod: 'Windows 11'}, {id: 2, prod: 'Windows 10'}, {id:3, prod: 'Microsoft 365'}, {id: 4, prod: 'Office 2022'}])
   return(
     <div className="listado">
       <ul className="list">
         {
-          masVendido.map((items) => (
-            <li key ={items.id} ><a href="#!" > <FontAwesomeIcon icon={faGlobe}/> {items.prod}</a></li>
+          masVendido.map((item) => (
+            <li key ={item.id} ><a href="#!" > <FontAwesomeIcon icon={faGlobe}/> {item.prod}</a></li>
           ))
         }
       </ul>
