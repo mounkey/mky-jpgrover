42e7385ffe1328d68aa875609c0686cb53d1b33d
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <jpgrover@mounkey.cl>
2022-06-30 15:09:21 -0400

    Ni idea

--- src/componets/Item.js
+++ src/componets/Item.js
@@ -1,41 +1,39 @@
 import React, { useState, useEffect} from "react";
 import '../App.css';
 import 'materialize-css/dist/css/materialize.css';
-import data from '../data/data';
+import data from '../data/index';
 
 const Item =() =>  {
-  
-  const Promise = new Promise((resolve, reject) => { 
+
+  const promise = new Promise((resolve, reject) => { 
     setTimeout(() => {
       resolve(data);
     }, 1000);
-  });
-  
+  });   
+
   const [swSell,setSwSell] = useState([]);
 
   useEffect(() => {
-    Promise.then(data => {
-      setSwSell(data);
+    promise.then(resolve => {
+      setSwSell(resolve);
     }).catch(error => {
-      console.log(error);
+      console.log(error); 
     }).finally(() => {
       console.log('finally');
     });
   }, []);
 
-  return(
+  return (
     <>
     {
-      swSell.map((item) => {
-        return(
-          <div id= {item.id}>
-            <h3>{item.name}</h3> - <small>{item.category}</small>
-            <img src= {item.picture} alt="Producto" className="itemImg" />
-            <p>{item.description}</p>
-            <p>{item.price}</p>
-          </div>          
-        )
-      })
+      swSell.map((item) => (
+        <div id= {item.id}>
+        <h3>{item.name}</h3> - <small>{item.category}</small>
+        <img src= {item.picture} alt="Producto" className="itemImg" />
+        <p>{item.description}</p>
+        <p>{item.price}</p>
+      </div>  
+      ))   
     }
     </>
   )
